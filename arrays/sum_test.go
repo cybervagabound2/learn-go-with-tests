d51e4c783068d47049b130af85dd8c152156ecf3
brodyaga <cybervagabound2@protonmail.com>
2018-11-17 00:43:53 -0500

    TestSumAll and SumAll working

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -21,7 +24,7 @@ func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1,2}, []int{0,9})
 	want := []int{3, 9}
 
-	if got != want {
-		t.Errorf("got %d want %v", got, want)
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
 }
